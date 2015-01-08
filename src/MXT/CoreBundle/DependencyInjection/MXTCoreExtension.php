dc78fbddd1ee02ca1bc1ad22b9080823085fd7d5
Chiarillo Massimo <info@max090.com>
2015-01-09 00:44:49 +0100

    Add Kickass Configuration

--- src/MXT/CoreBundle/DependencyInjection/MXTCoreExtension.php
+++ src/MXT/CoreBundle/DependencyInjection/MXTCoreExtension.php
@@ -24,5 +24,7 @@ class MXTCoreExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $loader->load('Torrent/Providers/kickass.yml');
     }
 }
