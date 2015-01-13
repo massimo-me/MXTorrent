04b53f93e987973e1c47060b5a2675c9149d7b81
Chiarillo Massimo <info@max090.com>
2015-01-13 20:21:14 +0100

    FIX#Scrutinizer Duplicate code in Bundle Extension

--- src/MXT/CoreBundle/DependencyInjection/MXTBaseExtension.php
+++ src/MXT/CoreBundle/DependencyInjection/MXTBaseExtension.php
@@ -7,15 +7,22 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class MXTCoreExtension extends MXTBaseExtension
+class MXTBaseExtension extends Extension
 {
+    /**
+     * @var Loader\YamlFileLoader
+     */
+    protected $loader;
+
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        parent::load($configs, $container);
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
 
-        $this->loader->load('Torrent/Providers/kickass.yml');
+        $this->loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $this->loader->load('services.yml');
     }
 }
--- src/MXT/TransmissionBundle/DependencyInjection/MXTTransmissionExtension.php
+++ src/MXT/TransmissionBundle/DependencyInjection/MXTTransmissionExtension.php
@@ -2,19 +2,15 @@
 
 namespace MXT\TransmissionBundle\DependencyInjection;
 
+use MXT\CoreBundle\DependencyInjection\MXTCoreExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class MXTTransmissionExtension extends Extension
+class MXTTransmissionExtension extends MXTCoreExtension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        parent::load($configs, $container);
     }
 }
